fc203d47ea5df5431a494327846fb58e1e7eede8
Pedro Henrique Passos Rocha <pedro.rocha@optsol.com.br>
2024-07-28 18:32:05 -0300

    Commit inicial

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import view.MainView;
+
+public class Main {
+    public static void main(String[] args) {
+        MainView mainView = new MainView();
+        mainView.setVisible(true);
+    }
+}
