bb0d32da72038f479321e6121f2c213aae6597eb
Catterina Vittorazzi Salvador <catterinavs@gmail.com>
2024-07-29 22:23:20 -0300

    feat: adicionando métodos para media e maximos e minimos

--- src/main/java/model/DadosMediosModel.java
+++ src/main/java/model/DadosMediosModel.java
@@ -4,10 +4,71 @@
  */
 package model;
 
+import java.util.List;
+
 /**
  *
  * @author Catterina Salvador
  */
 public class DadosMediosModel {
+    private Double temperaturaMedia;
+    private Double umidadeMedia;
+    private Double pressaoMedia;
+    private Long numeroRegistros;
+
+    public DadosMediosModel(Double temperaturaMedia, Double umidadeMedia, Double pressaoMedia, Long numeroRegistros) {
+        this.temperaturaMedia = temperaturaMedia;
+        this.umidadeMedia = umidadeMedia;
+        this.pressaoMedia = pressaoMedia;
+        this.numeroRegistros = numeroRegistros;
+    }
+
+    public Double getTemperaturaMedia() {
+        return temperaturaMedia;
+    }
+
+    public Double getUmidadeMedia() {
+        return umidadeMedia;
+    }
+
+    public Double getPressaoMedia() {
+        return pressaoMedia;
+    }
+
+    public Long getNumeroRegistros() {
+        return numeroRegistros;
+    }
+
+    public void setTemperaturaMedia(Double temperaturaMedia) {
+        this.temperaturaMedia = temperaturaMedia;
+    }
+
+    public void setUmidadeMedia(Double umidadeMedia) {
+        this.umidadeMedia = umidadeMedia;
+    }
+
+    public void setPressaoMedia(Double pressaoMedia) {
+        this.pressaoMedia = pressaoMedia;
+    }
+
+    public void setNumeroRegistros(Long numeroRegistros) {
+        this.numeroRegistros = numeroRegistros;
+    }
     
+    private void getMedias(List<Clima> registros){
+        var somaTemperaturas = 0; 
+        var somaPressoes = 0;
+        var somaUmidades = 0;
+        
+        for(Clima registro: registros){
+            somaTemperaturas += registro.getTemperatura();
+            somaUmidades += registro.getUmidade();
+            somaPressoes += registro.getPressao();
+        }
+        setTemperaturaMedia((somaTemperaturas / (double)registros.size()));
+        setPressaoMedia((somaPressoes / (double)registros.size()));
+        setUmidadeMedia((somaUmidades / (double)registros.size()));
+        setNumeroRegistros((long)registros.size());
+        
+    }
 }
--- src/main/java/model/MaxMinModel.java
+++ src/main/java/model/MaxMinModel.java
@@ -4,10 +4,92 @@
  */
 package model;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  *
  * @author Catterina Salvador
  */
 public class MaxMinModel {
+    private Double temperaturaMax;
+    private Double temperaturaMin;
+    private Double umidadeMax;
+    private Double umidadeMin;
+    private Double pressaoMax;
+    private Double pressaoMin;
+
+    public MaxMinModel(Double temperaturaMax, Double temperaturaMin, Double umidadeMax, Double umidadeMin, Double pressaoMax, Double pressaoaMin) {
+        this.temperaturaMax = temperaturaMax;
+        this.temperaturaMin = temperaturaMin;
+        this.umidadeMax = umidadeMax;
+        this.umidadeMin = umidadeMin;
+        this.pressaoMax = pressaoMax;
+        this.pressaoMin = pressaoMin;
+    }
+
+    public Double getTemperaturaMax() {
+        return temperaturaMax;
+    }
+
+    public Double getTemperaturaMin() {
+        return temperaturaMin;
+    }
+
+    public Double getUmidadeMax() {
+        return umidadeMax;
+    }
+
+    public Double getUmidadeMin() {
+        return umidadeMin;
+    }
+
+    public Double getPressaoMax() {
+        return pressaoMax;
+    }
+
+    public Double getPressaoaMin() {
+        return pressaoMin;
+    }
+
+    public void setTemperaturaMax(Double temperaturaMax) {
+        this.temperaturaMax = temperaturaMax;
+    }
+
+    public void setTemperaturaMin(Double temperaturaMin) {
+        this.temperaturaMin = temperaturaMin;
+    }
+
+    public void setUmidadeMax(Double umidadeMax) {
+        this.umidadeMax = umidadeMax;
+    }
+
+    public void setUmidadeMin(Double umidadeMin) {
+        this.umidadeMin = umidadeMin;
+    }
+
+    public void setPressaoMax(Double pressaoMax) {
+        this.pressaoMax = pressaoMax;
+    }
+
+    public void setPressaoMin(Double pressaoaMin) {
+        this.pressaoMin = pressaoaMin;
+    }
+    
+    private void getMaxMin(List<Clima> registros){
+        var temperaturas = registros.stream().map(registro -> registro.getTemperatura()).collect(Collectors.toList());
+        setTemperaturaMax(Collections.max(temperaturas));
+        setTemperaturaMin(Collections.min(temperaturas));
+        
+        var umidades = registros.stream().map(registro -> registro.getUmidade()).collect(Collectors.toList());
+        setUmidadeMax(Collections.max(umidades));
+        setUmidadeMin(Collections.min(umidades));
+        
+        var pressoes = registros.stream().map(registro -> registro.getPressao()).collect(Collectors.toList());
+        setPressaoMax(Collections.max(pressoes));
+        setPressaoMin(Collections.min(pressoes));
+    }
+    
     
 }
